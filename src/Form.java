1e98e3e8f34322f03777d5a656a48e3d6ec4d9f0
DJEzio <mariusadrian.popa@ulbsibiu.ro>
2024-03-12 09:31:06 +0200

    schimbare

---
+++ src/Form.java
@@ -0,0 +1,13 @@
+public class Form {
+    private String color;
+    public Form()
+    {
+        color = "white";
+    }
+    public String getColor() {
+        return color;
+    }
+    public void setColor(String color) {
+        this.color = color;
+    }
+}
